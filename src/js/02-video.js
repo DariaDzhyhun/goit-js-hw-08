00efac184453b6c3311d673175cfb1115c7e61c1
DariaDzhyhun <dzhigun21@gmail.com>
2023-03-20 14:18:03 +0200

    finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,24 @@
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframeEl = document.querySelector('#vimeo-player');
+const vimeoPlayer = new Player(iframeEl);
+
+vimeoPlayer.on('timeupdate', throttle(onPlay, 1000));
+setTime();
+
+function onPlay({ seconds, duration }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+  if (seconds === duration) {
+    localStorage.removeItem('videoplayer-current-time');
+  }
+}
+
+function setTime() {
+  if (localStorage.getItem('videoplayer-current-time')) {
+    vimeoPlayer.setCurrentTime(
+      localStorage.getItem('videoplayer-current-time')
+    );
+  } else {
+    vimeoPlayer.setCurrentTime(0);
+  }
+}
